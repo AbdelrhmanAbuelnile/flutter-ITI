33c49d244a548bf0460be70a81d5941bc7cfa0d7
Abdelrhman Hani <abdohani362@gmail.com>
2023-02-03 17:18:37 +0200

    first task

---
+++ lib/intro.dart
@@ -0,0 +1,16 @@
+// import 'package:flutter/material.dart';
+
+// class Person {
+//   String name;
+//   int age;
+//   // Person(this.name,this.age);
+//   Person({required this.name,required this.age});
+// }
+
+// void main (){
+//   // Person p = Person("haha", 21);
+//   Person p = Person(name: "manga", age: 21);
+//   MaterialApp(
+
+//   )
+// }
